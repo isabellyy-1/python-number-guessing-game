2d01f0fd7ee50a7ec9ba6fb35226033730b6cf8c
isabellyy-1 <isabellyy5343@gmail.com>
2026-02-28 02:52:32 -0300

    Enhance number guessing game with input validation and colors.

--- python-number-guessing-game .py
+++ python-number-guessing-game .py
@@ -1,9 +1,19 @@
 import random
+from time import sleep
 number = random.randint(0,5)
 guess = None
+print(20 * '\033[31m=\033[0m')
 while guess != number:
-    guess= int(input('Try to guess the number between 0 and 5 :'))
+    try:
+     guess= int(input('Try to guess the number between 0 and 5 :'))
+    except ValueError:
+        print('Type a valid number')
+        continue
+    print ("\033[35mLoading...\033[0m")
+    sleep(2)
     if guess == number:
         print('Congratulations!! You Guessed it!!!')
+        print(20 * '\033[31m=\033[0m')
     else:
         print('Wrong Guess. Try Again!!! ')
+        print(20 * '\033[31m=\033[0m')
