edaf096b3130450bb6de11a0261130ca2b650323
isabellyy-1 <isabellyy5343@gmail.com>
2026-03-10 20:32:56 -0300

    add attempts counter and colors to number guessing game

--- python-number-guessing-game .py
+++ python-number-guessing-game .py
@@ -1,19 +1,28 @@
-import random
 from time import sleep
-number = random.randint(0,5)
-guess = None
-print(20 * '\033[31m=\033[0m')
-while guess != number:
+import random
+red = '\033[31m'
+reset = '\033[0m'
+blue = '\033[34m'
+purple = '\033[35m'
+print('=~'*20)
+print(f'{purple}Number Guessing Game2.0{reset}')
+print('=~'*20)
+guess = 0
+attempts = 0
+computer_number = random.randint(1,10)
+while guess != computer_number:
+    attempts += 1
     try:
-     guess= int(input('Try to guess the number between 0 and 5 :'))
+        guess = int(input(f'{blue}Try to Guess Number Between 1,10:{reset}'))
     except ValueError:
-        print('Type a valid number')
+        print(f'{red}Type a Valid Number{reset}')
         continue
-    print ("\033[35mLoading...\033[0m")
+    print(f'{red}Loading...{reset}')
     sleep(2)
-    if guess == number:
-        print('Congratulations!! You Guessed it!!!')
-        print(20 * '\033[31m=\033[0m')
+    '\n'
+    if guess == computer_number:
+        print(f'You Guessed Correct in {attempts} attempts ')
+        print('=~' * 20)
     else:
-        print('Wrong Guess. Try Again!!! ')
-        print(20 * '\033[31m=\033[0m')
+        print(f'Wrong Guess. Try Again!!!{attempts} attempts')
+        print('=~' * 20)
